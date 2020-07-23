c6a6f58cea28c4df1408ed98c1dc2975fbc74a2b
Cory LaViska <cory@abeautifulsite.net>
2020-07-23 10:16:19 -0400

    Update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,25 @@
 import path from 'path';
 import commonjs from '@rollup/plugin-commonjs';
-import copy from 'rollup-plugin-copy'
+import copy from 'rollup-plugin-copy';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  output: [
-    { dir: path.resolve('dist/'), format: 'cjs' }
-  ],
+  output: [{ dir: path.resolve('dist/'), format: 'es' }],
   plugins: [
     resolve(),
     commonjs(),
     postcss({
-      extensions: [ '.css' ]
+      extensions: ['.css']
     }),
     copy({
       targets: [
-        { 
-          src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/shoelace/icons'), 
-          dest: path.resolve(__dirname, 'dist/icons')
-        },
+        {
+          src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/shoelace/icons'),
+          dest: path.resolve(__dirname, 'dist')
+        }
       ]
-    })    
+    })
   ]
 };
